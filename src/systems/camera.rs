b73b35dd1acd663f86920c99fd7abc115835f591
Ricardo Delfin <me@rdelfin.com>
2021-05-02 19:39:42 +0100

    Added positional following as well

--- src/systems/camera.rs
+++ src/systems/camera.rs
@@ -35,6 +35,8 @@ impl<'s> System<'s> for CameraTrackSystem {
             }
         };
 
+        camera_transform.set_translation_x(tracked_position.x);
+        camera_transform.set_translation_z(tracked_position.z - 10.);
         camera_transform.face_towards(tracked_position, Vector3::new(0.0, 1.0, 0.0));
     }
 }
