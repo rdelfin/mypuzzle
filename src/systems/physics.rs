df05812a6aeecc033ba51ca5f4f7bfa22f5bb419
Ricardo Delfin <me@rdelfin.com>
2021-05-02 19:24:41 +0100

    Added gravity to physics system

--- src/systems/physics.rs
+++ src/systems/physics.rs
@@ -1,4 +1,4 @@
-use crate::components::{Position, Velocity};
+use crate::components::{Position, Velocity, Weight};
 use amethyst::{
     core::{Time, Transform},
     derive::SystemDesc,
@@ -12,15 +12,36 @@ impl<'s> System<'s> for PhysicsSystem {
     type SystemData = (
         WriteStorage<'s, Transform>,
         WriteStorage<'s, Position>,
-        ReadStorage<'s, Velocity>,
+        WriteStorage<'s, Velocity>,
+        ReadStorage<'s, Weight>,
         Read<'s, Time>,
     );
 
-    fn run(&mut self, (mut transforms, mut positions, velocities, time): Self::SystemData) {
-        for (transform, position, velocity) in (&mut transforms, &mut positions, &velocities).join()
+    fn run(
+        &mut self,
+        (mut transforms, mut positions, mut velocities, weight, time): Self::SystemData,
+    ) {
+        for (transform, position, velocity, weight) in (
+            &mut transforms,
+            &mut positions,
+            (&mut velocities).maybe(),
+            (&weight).maybe(),
+        )
+            .join()
         {
             let frame_delta_s = time.fixed_time().as_secs_f32();
-            position.p += frame_delta_s * velocity.v;
+            if let Some(velocity) = velocity {
+                if let Some(weight) = weight {
+                    if position.p.y > 1.0 {
+                        velocity.v.y -= weight.g * frame_delta_s;
+                    } else {
+                        velocity.v.y = 0.0;
+                        position.p.y = 1.0;
+                    }
+                }
+
+                position.p += frame_delta_s * velocity.v;
+            }
             transform.set_translation_xyz(position.p.x, position.p.y, position.p.z);
         }
     }
